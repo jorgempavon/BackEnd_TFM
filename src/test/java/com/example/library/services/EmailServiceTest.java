d235eb8593af07d003e8510fd5d991933a999caa
jorgempavon <jorge.mpavon@alumnos.upm.es>
2025-06-13 16:05:15 +0200

    added tests for email service in issue#12

--- src/test/java/com/example/library/services/EmailServiceTest.java
+++ src/test/java/com/example/library/services/EmailServiceTest.java
@@ -23,6 +23,7 @@ public class EmailServiceTest {
     private EmailService emailService;
     private final String EXAMPLE_NAME = "example";
     private final String EXAMPLE_EMAIL = "test@example.com";
+    private final String EXAMPLE_OLD_EMAIL = "oldTest@example.com";
     private final String EXAMPLE_PASSWORD = "pass123";
     @Test
     void newAccountEmail_successful(){
@@ -33,6 +34,24 @@ public class EmailServiceTest {
     void deleteAccountEmail_successful(){
         doNothing().when(mailSender).send(any(SimpleMailMessage.class));
         this.emailService.deleteAccountEmail(EXAMPLE_EMAIL,EXAMPLE_NAME);
+    }
+
+    @Test
+    void oldAccountEmail_successful(){
+        doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+        this.emailService.oldAccountEmail(EXAMPLE_OLD_EMAIL,EXAMPLE_EMAIL,EXAMPLE_NAME);
+    }
+
+    @Test
+    void modifiedAccountEmail_successful(){
+        doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+        this.emailService.modifiedAccountEmail(EXAMPLE_OLD_EMAIL,EXAMPLE_EMAIL,EXAMPLE_NAME,EXAMPLE_PASSWORD);
+    }
+
+    @Test
+    void regeneratedPasswordEmail_successful(){
+        doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+        this.emailService.regeneratedPasswordEmail(EXAMPLE_EMAIL,EXAMPLE_NAME,EXAMPLE_PASSWORD);
     }
     @Test
     void newAccountEmail_whenNotExistsEmail_throwBadRequestException(){
@@ -51,6 +70,33 @@ public class EmailServiceTest {
                 .send(any(SimpleMailMessage.class));
         assertThrows(BadRequestException.class, () ->
                 emailService.deleteAccountEmail(EXAMPLE_EMAIL, EXAMPLE_NAME)
+        );
+    }
+    @Test
+    void oldAccountEmail_whenNotExistsEmail_throwBadRequestException(){
+        doThrow(new BadRequestException("El correo proporcionado no existe"))
+                .when(mailSender)
+                .send(any(SimpleMailMessage.class));
+        assertThrows(BadRequestException.class, () ->
+                emailService.oldAccountEmail(EXAMPLE_OLD_EMAIL,EXAMPLE_EMAIL, EXAMPLE_NAME)
+        );
+    }
+    @Test
+    void modifiedAccountEmail_whenNotExistsEmail_throwBadRequestException(){
+        doThrow(new BadRequestException("El correo proporcionado no existe"))
+                .when(mailSender)
+                .send(any(SimpleMailMessage.class));
+        assertThrows(BadRequestException.class, () ->
+                emailService.modifiedAccountEmail(EXAMPLE_OLD_EMAIL,EXAMPLE_EMAIL, EXAMPLE_NAME,EXAMPLE_PASSWORD)
+        );
+    }
+    @Test
+    void regeneratedPasswordEmail_whenNotExistsEmail_throwBadRequestException(){
+        doThrow(new BadRequestException("El correo proporcionado no existe"))
+                .when(mailSender)
+                .send(any(SimpleMailMessage.class));
+        assertThrows(BadRequestException.class, () ->
+                emailService.regeneratedPasswordEmail(EXAMPLE_EMAIL, EXAMPLE_NAME,EXAMPLE_PASSWORD)
         );
     }
 }
